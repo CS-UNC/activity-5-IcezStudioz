e7984e3c672121881423ba5af0ba3e0da3d0e544
IZAYAH <hesl8572@bears.unco.edu>
2025-11-05 21:06:33 +0000

    in progress 2

--- activity5.py
+++ activity5.py
@@ -1,27 +1,25 @@
-words_file = open('CROSSWD.txt','r')
-
 def more_than_20(file):
-    count = 0
-    for file in words_file:
-        if len(file) > 20:
-            count += 1
-            print (file)
+    open(file)
+    for i in file:
+        if len(i) > 20:
+            print (i)
 
 def has_no_e(word):
-    for word in words_file:
-        if 'e' not in word:
-            print(True)
-        else:
+    for z in word:
+        if 'e' in word:
             print(False)
+        else:
+            print(True)
 
 def uses_only(word,letters):
-    for word in words_file:
+    for word in letters:
         if letters not in word:
             print(False)
         else:
             print(True)
 
 def all_uses_only(file, letters):
-    for file in words_file:
+    uses_only(word,letters)
+    for y in file:
         if letters in file:
-            print (file)
+            print (y)
