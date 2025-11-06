e1caba40962d09c3f59989fd2aa2a8389bb2ed7b
IZAYAH <hesl8572@bears.unco.edu>
2025-11-06 01:09:29 +0000

    complete

--- activity5.py
+++ activity5.py
@@ -1,25 +1,30 @@
-def more_than_20(file):
-    open(file)
-    for i in file:
-        if len(i) > 20:
-            print (i)
+def more_than_20(file_name):
+    with open (file_name, "r") as file:
+        h = []
+        for i in file:
+            i = i.strip()
+            if len(i) > 20:
+                h.append(i)
+    return h
 
 def has_no_e(word):
+    result = True
     for z in word:
-        if 'e' in word:
-            print(False)
-        else:
-            print(True)
+        if z == "e":
+            result = False
+    return result
 
 def uses_only(word,letters):
-    for letters in word:
-        if letters not in word:
-            print(False)
-        else:
-            print(True)
+    for letter in word:
+        if letter not in letters:
+            return False
+    return True
 
-def all_uses_only(file, letters):
-    uses_only(word,letters)
-    for y in file:
-        if letters in file:
-            print (y)
+def all_uses_only(file,letters):
+    with open (file, "r") as f:
+        goodWords = []
+        for word in f:
+            word = word.strip()
+            if uses_only(word,letters):
+                goodWords.append(word)
+    return goodWords
