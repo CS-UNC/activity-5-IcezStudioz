0be41b4c99cbdf35a475e601d326ea082b2cda1d
IZAYAH <hesl8572@bears.unco.edu>
2025-11-10 19:04:18 +0000

    extra

---
+++ collections.py
@@ -0,0 +1,7 @@
+prices = {'Eggs': 0.5, 'Flour': 0.05, 'Sugar': 0.10, 'Milk': 2.49}
+eggs_total = 6 * prices['Eggs']
+flour_total = 500 * prices['Flour']
+sugar_total = 400 * prices['Milk']
+milk_total = 3 * prices['Sugar']
+total = eggs_total + flour_total + sugar_total + milk_total
+print(total)
